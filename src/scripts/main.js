fca0f7cc586c59d5b2c318a8c21e820a0bd54a23
Ronaldo Hauser Dias <duqueronaldo@icloud.com>
2023-10-16 12:41:41 -0300

    pausando o projeto para debug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,28 @@
-AOS.init();
 
-const contador = document.getElementById('contador');
-const dataDoEvento = new Date('oct 10, 2023 19:00:00');
-const timeStampDoEvento = dataDoEvento.getTime();
+const eventData = new Date("Mar 13, 2024 18:00:00");
+const eventDataTimeStamp = eventData.getTime();
+const elem = document.getElementById('niverData');
 
-const contaAsHoras = setInterval(() => {
-    const agora = new Date();
-    const timeStampAtual = agora.getTime();
+//Set interval, trigger function each 1 second
+const timeOffset = setInterval(() => {
+    const currentTime = new Date();
+    const currentTimestamp = currentTime.getTime();
+    const timeOffsetBetweenDates = eventDataTimeStamp - currentTimestamp;
+    const daysInMs = 1000 * 3600 * 24;
+    const hoursInMs = 1000 * 3600;
+    const minutesInMs = 1000 * 60;
+    //getDays
+    const daysToEvent   = Math.floor(timeOffsetBetweenDates / daysInMs);
+    //getHours
+    const hoursToEvent  = Math.floor((timeOffsetBetweenDates % daysInMs) / hoursInMs);
+    //getMinutes
+    const minutesToEvent  = Math.floor((timeOffsetBetweenDates % hoursInMs) / minutesInMs);
+    //getSeconds
+    const secondsToEvent  = Math.floor((timeOffsetBetweenDates % minutesInMs) / 1000);
+    elem.innerHTML = `${daysToEvent}d ${hoursToEvent}h ${minutesToEvent}m ${secondsToEvent}s `;
 
-    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
-    const diaEmMs = 1000 * 60 * 60 * 24;
-    const horaEmMs = 1000 * 60 * 60;
-    const minutoEmMs = 1000 * 60;
-
-    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
-    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
-    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
-
-    contador.innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
-
-    if(diasAteOEvento < 0) {
-        clearInterval(contaAsHoras);
-        contador.innerHTML = "Evento expirado"
+    if(timeOffsetBetweenDates < 0) {
+        clearInterval(timeOffset);
+        elem.innerHTML = "Já ta Sendo Celebrado, Vamos lá 🎉";
     }
 }, 1000);
